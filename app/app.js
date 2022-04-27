dad0d3b66beadcc5c75583acf7d425f7219ad2f8
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-04-27 13:51:31 +0900

    돔을 이용하여 HTML객체 저장

--- app/app.js
+++ app/app.js
@@ -27,6 +27,7 @@ const home = require("./src/routes/home")
 //앱 세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 //라우팅
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,21 @@
 "use strict";
 
+//DOM -> html에 존재하는 값을 단순히 자바스크립트에서 제어할 수 없지만 이를 가능하게 해주는 것이
+// DOM객체이다. 문서 객체모델
+
+const id = document.querySelector("#id"),
+pwd = document.querySelector("#pwd"),
+loginBtn = document.querySelector("button");
+console.log(id);
 console.log("hello");
-console.log("bye");
+
+loginBtn.addEventListener("click",login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pwd: pwd.value,
+    };
+
+    console.log(req);
+};
